742a712ff02ebb3fac7d34f259cab58f6c2a6524
magicaldave <smokydave@icloud.com>
2023-03-05 22:29:36 -0600

    Edit: Added call to ExplodeFace method when entering the Win zone, and added code to grab the Timer UI.

--- 0x00-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
+++ 0x00-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -5,18 +5,24 @@ using UnityEngine.UI;
 
 public class WinTrigger : MonoBehaviour
 {
-	public Text timeUI;
+	private Text timeUI;
 	private GameObject Player;
 	private Timer timeScript;
 
 	void Start()
 	{
-		Player = GameObject.Find("Flat Stanley");
+		Player = GameObject.Find("Player");
+		timeUI = GameObject.Find("TimerText").GetComponent<Text>();
 		timeScript = Player.GetComponent<Timer>();
 	}
 
 	void OnTriggerEnter()
 	{
+
+		// Run the ExplodeFace Method from the PlayerController
+		Player.GetComponent<PlayerController>().ExplodeFace();
+
+		// Blow up the timer text, then stop the script
 		timeUI.color = Color.green;
 		timeUI.fontSize = 60;
 		Destroy(timeScript);
