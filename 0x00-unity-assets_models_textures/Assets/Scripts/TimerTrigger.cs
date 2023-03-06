d3eaa8f6449116e56c45f83011316f3c5e249a5d
magicaldave <smokydave@icloud.com>
2023-03-05 22:30:31 -0600

    Edit: Grabbed component on exit instead of on start. Probably bad optimization.

--- 0x00-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
+++ 0x00-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -1,22 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TimerTrigger : MonoBehaviour
 {
-	public GameObject Player;
-	private Timer timeScript;
-
-	// Start is called before the first frame update
-	void Start()
-	{
-		timeScript = Player.GetComponent<Timer>();
-	}
-
-	// Update is called once per frame
 	void OnTriggerExit(Collider other)
 	{
-		timeScript.enabled = true;
+
+		// Find and enable the Timer script once the player starts moving
+		GameObject.Find("Player").GetComponent<Timer>().enabled = true;
+
+		// Destroy this object once its job is done
 		Destroy(gameObject);
+
 	}
 }
